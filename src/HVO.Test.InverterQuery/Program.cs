edb1c693983538a563ba70c25a7069a5f559c75e
Roy Salisbury <roys@express-is.net>
2022-12-26 15:51:12 -0700

    before the system crashes

--- src/HVO.Test.InverterQuery/Program.cs
+++ src/HVO.Test.InverterQuery/Program.cs
@@ -1,4 +1,6 @@
 ﻿using HVO.Hardware.PowerSystems.Voltronic;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace HVO.Test.InverterQuery
 {
@@ -6,6 +8,21 @@ namespace HVO.Test.InverterQuery
     {
         static async Task Main(string[] args)
         {
+            var services = new ServiceCollection();
+            
+            services
+                .AddSingleton<InverterClient>()
+                .
+                .BuildServiceProvider();
+        }
+
+     
+        public void CreateHostBuilder23()
+        {
+
+        
+
+
             //using var client = new InverterCommunicationsClient();
 
             //var request1 = new InverterGetSerialNumberRequest();
@@ -16,49 +33,52 @@ namespace HVO.Test.InverterQuery
             //var result1 = await client.SendRequest<InverterGetSerialNumberResponse>(request1);
             //var result2 = await client.SendRequest<InverterGetDeviceProtocolIDResponse>(request2);
 
-            using var client = new InverterClient();
+            using var client = new InverterClient(null, 
             client.Open();
 
             var r1 = await client.QPI();
-            var r2 = await client.QID();
-            var r3 = await client.QSID();
-            var r4 = await client.QVFW();
-            var r5 = await client.QVFW2(); //NAK
-            var r6 = await client.QVFW3();
-            var r7 = await client.VERFW(); // NAK
-            var r8 = await client.QPIRI();
-            var r9 = await client.QFLAG();
-            var r10 = await client.QPIGS();
-            var r11 = await client.QMOD();
-            var r12 = await client.QPIWS();
-            var r13 = await client.QDI();
+            // var r2 = await client.QID();
+            // var r3 = await client.QSID();
+            // var r4 = await client.QVFW();
+            // var r5 = await client.QVFW2(); //NAK
+            // var r6 = await client.QVFW3();
+            // var r7 = await client.VERFW(); // NAK
+            // var r8 = await client.QPIRI();
+            // var r9 = await client.QFLAG();
+            // var r10 = await client.QPIGS();
+            // var r11 = await client.QMOD();
+            // var r12 = await client.QPIWS();
+            // var r13 = await client.QDI();
 
-            var r14 = await client.QMCHGCR();
-            var r15 = await client.QMUCHGCR();
-            var r16 = await client.QOPPT();
-            var r17 = await client.QCHPT();
+            // var r14 = await client.QMCHGCR();
+            // var r15 = await client.QMUCHGCR();
+            // var r16 = await client.QOPPT();
+            // var r17 = await client.QCHPT();
+            // var r19 = await client.QMN();
+            // var r20 = await client.QGMN();
+            // var r21 = await client.QBEQI();
+
+
+            // var r22 = await client.QET();
+            // var r23 = await client.QEY();
+            // var r24 = await client.QEM();
+            // var r25 = await client.QED();
+            // var r26 = await client.QLT();
+            // var r27 = await client.QLY();
+            // var r28 = await client.QLM();
+            // var r29 = await client.QLD();
+            // var r30 = await client.QLED();
+            // var r31 = await client.Q1();
+            // var r32 = await client.QBOOT();
+            // var r33 = await client.QOPM();
+            // var r34 = await client.QPGS();
+            // var r35 = await client.QBV();
+
+
             var r18 = await client.QT();
-            var r19 = await client.QMN();
-            var r20 = await client.QGMN();
-            var r21 = await client.QBEQI();
+            var r36 = await client.DAT(DateTime.Now);
 
-
-            var r22 = await client.QET();
-            var r23 = await client.QEY();
-            var r24 = await client.QEM();
-            var r25 = await client.QED();
-            var r26 = await client.QLT();
-            var r27 = await client.QLY();
-            var r28 = await client.QLM();
-            var r29 = await client.QLD();
-            var r30 = await client.QLED();
-            var r31 = await client.Q1();
-            var r32 = await client.QBOOT();
-            var r33 = await client.QOPM();
-            var r34 = await client.QPGS();
-            var r35 = await client.QBV();
-
-            //var r0 = await client.Test();
+//            var r0 = await client.Test();
 
 
 
