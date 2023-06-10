a76854f155b039209fa90d6dc0e01962d434c859
Roy Salisbury <roys@express-is.net>
2023-06-09 18:21:19 -0700

    .

--- src/HVO.Test.BLE/Program.cs
+++ src/HVO.Test.BLE/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Linux.Bluetooth;
@@ -11,118 +13,235 @@ namespace HVO.Test.BLE
     {
         static TimeSpan timeout = TimeSpan.FromSeconds(15);
 
+
+        public const string deviceFilter = "C8:47:8C:E4:54:B1";
+        public const string _serviceUUID = "0000ffe0-0000-1000-8000-00805f9b34fb";
+
         static async Task Main(string[] args)
         {
-            if (args.Length < 1)
-            {
-                Console.WriteLine("Usage: PrintDeviceInfo <deviceAddress> [adapterName]");
-                Console.WriteLine("Example: PrintDeviceInfo AA:BB:CC:11:22:33 hci0");
-                Console.WriteLine("Adapters:");
+            Adapter adapter = await BlueZManager.GetAdapterAsync("hci0", false);
 
-                var tmp = await BlueZManager.GetAdaptersAsync();
-                foreach (var a in tmp)
-                {
-                    Console.WriteLine($"- {a.Name}");
+            var adapterPath = adapter.ObjectPath.ToString();
+            var adapterName = adapterPath[(adapterPath.LastIndexOf("/") + 1)..];
+            Console.WriteLine($"Using Bluetooth adapter {adapterName}");
+
+            adapter.PoweredOn += Adapter_PoweredOnAsync;
+            adapter.DeviceFound += Adapter_DeviceFoundAsync;
+
+            Console.WriteLine("Waiting for events. Use Control-C to quit.");
+            Console.WriteLine();
+            await Task.Delay(-1);
+        }
+
+        private static async Task<string> GetDeviceDescriptionAsync(IDevice1 device)
+        {
+            var deviceProperties = await device.GetAllAsync();
+            return $"{deviceProperties.Address} (Alias: {deviceProperties.Alias}, RSSI: {deviceProperties.RSSI})";
+        }
+
+        private static async Task Adapter_PoweredOnAsync(Adapter sender, BlueZEventArgs e)
+        {
+            try
+            {
+                if (e.IsStateChange) {
+                    Console.WriteLine("Bluetooth adapter powered on.");
+                } else {
+                    Console.WriteLine("Bluetooth adapter already powered on.");
                 }
 
-                return;
+                Console.WriteLine("Starting scan...");
+                await sender.StartDiscoveryAsync();
             }
-
-            var deviceAddress = args[0];
-
-            IAdapter1 adapter;
-            if (args.Length > 1)
+            catch (Exception ex)
             {
-                // FALSE: 'hci0', TRUE: '/org/bluez/hci0'
-                var fullName = args[1].Contains("/org/bluez/");
-                adapter = await BlueZManager.GetAdapterAsync(args[1], fullName);
+                Console.Error.WriteLine(ex);
             }
-            else
+        }
+
+        private static async Task Adapter_DeviceFoundAsync(Adapter sender, DeviceFoundEventArgs e)
+        {
+            try
             {
-                var adapters = await BlueZManager.GetAdaptersAsync();
-                if (adapters.Count == 0)
-                {
-                    throw new Exception("No Bluetooth adapters found.");
+                var device = e.Device;
+
+                var deviceDescription = await GetDeviceDescriptionAsync(device);
+                
+                if (e.IsStateChange) {
+                    //Console.WriteLine($"Found: [NEW] {deviceDescription}");
+                } else {
+                    //Console.WriteLine($"Found: {deviceDescription}");
                 }
-                else
+
+                var deviceAddress = await device.GetAddressAsync();
+                var deviceName = await device.GetAliasAsync();
+                if (deviceAddress.Equals(deviceFilter, StringComparison.OrdinalIgnoreCase) || deviceName.Contains(deviceFilter, StringComparison.OrdinalIgnoreCase))
                 {
-                    foreach (var a in adapters)
+                    Console.WriteLine($"Requested device '{deviceAddress}' found. Stopping scan....");
+                    try
                     {
-                        var name = a.GetNameAsync();
-                        Console.WriteLine($"  - Adapter: '{name}' - '{a.ObjectPath.ToString()}'");
+                        await sender.StopDiscoveryAsync();
+                        Console.WriteLine("Scan stopped.");
+                    }
+                    catch (Exception ex)
+                    {
+                        // Best effort. Sometimes BlueZ gets in a state where you can't stop the scan.
+                        Console.Error.WriteLine($"Error stopping scan: {ex.Message}");
+                    }
+
+                    device.Connected += Device_ConnectedAsync;
+                    device.Disconnected += Device_DisconnectedAsync;
+                    device.ServicesResolved += Device_ServicesResolvedAsync;
+
+                    Console.WriteLine($"Connecting to {await device.GetAddressAsync()}...");
+                    await device.ConnectAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+
+        private static async Task Device_ConnectedAsync(Device sender, BlueZEventArgs e)
+        {
+            try
+            {
+                if (e.IsStateChange) {
+                    Console.WriteLine($"Connected to {await sender.GetAddressAsync()}");
+                } else {
+                    Console.WriteLine($"Already connected to {await sender.GetAddressAsync()}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+
+        private static async Task Device_DisconnectedAsync(Device sender, BlueZEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine($"Disconnected from {await sender.GetAddressAsync()}");
+                await Task.Delay(TimeSpan.FromSeconds(15));
+
+                Console.WriteLine($"Attempting to reconnect to {await sender.GetAddressAsync()}...");
+                await sender.ConnectAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+
+        private static async Task Device_ServicesResolvedAsync(Device sender, BlueZEventArgs e)
+        {
+            try
+            {
+                if (e.IsStateChange) {
+                    Console.WriteLine($"Services resolved for {await sender.GetAddressAsync()}");
+                } else {
+                    Console.WriteLine($"Services already resolved for {await sender.GetAddressAsync()}");
+                }
+
+                var servicesUUIDs = await sender.GetUUIDsAsync();
+
+                Console.WriteLine($"Device offers {servicesUUIDs.Length} service(s).");
+                // foreach (var uuid in servicesUUIDs)
+                // {
+                //   Console.WriteLine(uuid);
+                // }
+
+                var service = await sender.GetServiceAsync(_serviceUUID);
+                if (service == null)
+                {
+                    Console.WriteLine($"Service UUID {_serviceUUID} not found. Do you need to pair first?");
+                    return;
+                }
+
+                GattCharacteristic writeCharacteristic = null;
+                GattCharacteristic notifyCharacteristic = null;
+
+                var characteristics = await service.GetCharacteristicsAsync();
+                Console.WriteLine($"Service offers {characteristics.Count} characteristics(s).");
+                foreach (var item in characteristics)
+                {
+                    var flags = await item.GetFlagsAsync();
+                    if ((writeCharacteristic == null) && flags.Intersect(new [] {"write", "write-without-response" }).Any())
+                    {
+                        writeCharacteristic = await service.GetCharacteristicAsync(await item.GetUUIDAsync());
+                        Console.WriteLine($"Write Characteristic: {await item.GetUUIDAsync()}");
+                    }
+
+                    if ((notifyCharacteristic == null) && flags.Intersect(new[] { "notify" }).Any())
+                    {
+                        notifyCharacteristic = await service.GetCharacteristicAsync(await item.GetUUIDAsync()); 
+                        Console.WriteLine($"Notify Characteristic: {await item.GetUUIDAsync()}");
+
+                        notifyCharacteristic.Value += NotifyCharacteristic_Value;
+                    }
+
+                    if ((writeCharacteristic != null) && (notifyCharacteristic != null))
+                    {
+                        break;
                     }
                 }
 
-                adapter = adapters.First();
-            }
-
-            var adapterPath = adapter.ObjectPath.ToString();
-            var adapterName = adapterPath.Substring(adapterPath.LastIndexOf("/") + 1);
-            Console.WriteLine($"Using Bluetooth adapter {adapterName}");
-
-            // Find the Bluetooth peripheral.
-            var device = await adapter.GetDeviceAsync(deviceAddress);
-            if (device == null)
-            {
-                Console.WriteLine($"Bluetooth peripheral with address '{deviceAddress}' not found. Use `bluetoothctl` or Bluetooth Manager to scan and possibly pair first.");
-                return;
-            }
-
-            Console.WriteLine("Connecting...");
-            await device.ConnectAsync();
-            await device.WaitForPropertyValueAsync("Connected", value: true, timeout);
-            var deviceObjPath = device.ObjectPath.ToString();
-            Console.WriteLine($"Connected ({deviceObjPath}).");
-
-            Console.WriteLine("Waiting for services to resolve...");
-            await device.WaitForPropertyValueAsync("ServicesResolved", value: true, timeout);
-
-            var servicesUUID = await device.GetUUIDsAsync();
-            Console.WriteLine($"Device offers {servicesUUID.Length} service(s).");
-            if (servicesUUID is not null)
-            {
-                foreach (var svc in servicesUUID)
+                if (writeCharacteristic != null)
                 {
-                    Console.WriteLine($"- Uuid: {svc}");
+                    var getInfo = new byte[] { 0xAA, 0x55, 0x90, 0xEB, 0x97, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x11 };
+                    var options = new Dictionary<string, object>();
+                    await writeCharacteristic.WriteValueAsync(getInfo, options);
+                }
+
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+
+        private static async Task NotifyCharacteristic_Value(GattCharacteristic sender, GattCharacteristicValueEventArgs e)
+        {
+            try
+            {
+                var uuid = await sender.GetUUIDAsync();
+                Console.WriteLine($"Notify Sender: {uuid}, \tLength: {e.Value.Length} \tValue: {BitConverter.ToString(e.Value)}");
+
+                var response = AssembleResponseValue(sender, e.Value, 320);
+                if (response.Item1 == true)
+                {
+                    Console.WriteLine($"=====Value: {BitConverter.ToString(response.Item2)}");
                 }
             }
-
-            var deviceInfoServiceFound = servicesUUID.Any(uuid => String.Equals(uuid, GattConstants.DeviceInformationServiceUUID, StringComparison.OrdinalIgnoreCase));
-            if (!deviceInfoServiceFound)
+            catch (Exception ex)
             {
-                Console.WriteLine("Device doesn't have the Device Information Service. Try pairing first?");
-                return;
+                Console.Error.WriteLine(ex);
+            }
+        }
+
+        private static byte[] responseBuffer = new byte[0];
+
+        private static (bool, byte[]) AssembleResponseValue(GattCharacteristic sender, byte[] value, int minLength)
+        {
+            // Flush buffer on every preamble
+            if ((value[0] == 0x55) && (value[1] == 0xAA) && (value[2] == 0xEB) && (value[3] == 0x90))
+            {
+                responseBuffer = value;
+            } else {
+                responseBuffer = responseBuffer.Concat(value).ToArray();
             }
 
-            // Console.WriteLine("Retrieving Device Information service...");
-            var service = await device.GetServiceAsync(GattConstants.DeviceInformationServiceUUID);
-            var modelNameCharacteristic = await service.GetCharacteristicAsync(GattConstants.ModelNameCharacteristicUUID);
-            var manufacturerCharacteristic = await service.GetCharacteristicAsync(GattConstants.ManufacturerNameCharacteristicUUID);
-
-            int characteristicsFound = 0;
-            if (modelNameCharacteristic != null)
+            if (responseBuffer.Length < minLength)
             {
-                characteristicsFound++;
-                Console.WriteLine("Reading model name characteristic...");
-                var modelNameBytes = await modelNameCharacteristic.ReadValueAsync(timeout);
-                Console.WriteLine($"Model name: {Encoding.UTF8.GetString(modelNameBytes)}");
+                return (false, null);
             }
 
-            if (manufacturerCharacteristic != null)
-            {
-                characteristicsFound++;
-                Console.WriteLine("Reading manufacturer characteristic...");
-                var manufacturerBytes = await manufacturerCharacteristic.ReadValueAsync(timeout);
-                Console.WriteLine($"Manufacturer: {Encoding.UTF8.GetString(manufacturerBytes)}");
-            }
+            var result = (true, responseBuffer.ToArray());
+            responseBuffer = new byte[0];
 
-            if (characteristicsFound == 0)
-            {
-                Console.WriteLine("Model name and manufacturer characteristics not found.");
-            }
-
-            await device.DisconnectAsync();
-            Console.WriteLine("Disconnected.");
+            return result;
         }
     }
 }
